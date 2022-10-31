51bb37b98b6ba7c2ea8e38b51c1ac076f50cb7e0
sharpsailor <sharpsailor13@gmail.com>
2022-10-31 06:44:36 +0530

    setup routes

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+// ROUTES
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}...`));
