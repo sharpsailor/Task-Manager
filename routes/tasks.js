941e45e284eff295c3b02336123c0c8200dbc5ae
sharpsailor <sharpsailor13@gmail.com>
2022-11-03 22:08:28 +0530

    heroku

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,17 +1,17 @@
 const express = require("express");
-// const router = express.Router();
-const app = express();
+const router = express.Router();
+
 const {
   getAllTasks,
   createTask,
   updateTask,
   deleteTask,
-  getTask,
+  getTask
 } = require("../controller/tasks");
-app.route("/").get(getAllTasks).post(createTask);
-app.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
-module.exports = app;
+module.exports = router;
 
 //GET  Method used to retrieve data from server . This is only read-only method,no mutating nor corrupting data
 // POST new data entry from parent data
