5e58d8adb6ec52f99dbceba5857cb9bc0059f53e
sharpsailor <sharpsailor13@gmail.com>
2023-03-19 20:35:36 +0530

    Project started

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
-require("dotenv/config");
+const dotenv = require("dotenv");
+dotenv.config();
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 // Middleware
